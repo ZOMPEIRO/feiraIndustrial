a28819df343d64d89a34b85c79542ab3e6b0d09f
Felipe Zompero <138314639+ZOMPEIRO@users.noreply.github.com>
2024-11-05 10:43:22 -0300

    Projeto feira industrial

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let cont = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval (function(){
+    nextImage();
+
+}, 4000)
+
+function nextImage(){
+    cont++;
+    if(cont > 4)
+    {
+        cont = 1;
+    }
+
+    document.getElementById("radio"+cont).checked = true;
+
+}
+
+
